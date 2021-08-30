37de92c49c1e25acbe5da45b5ccc355ca1101f15
jeppe-t <jeppethylstrup@yahoo.dk>
2021-08-30 15:05:52 +0200

    Changes

--- Text adventure/src/Controller.java
+++ Text adventure/src/Controller.java
@@ -2,47 +2,73 @@ import java.util.Scanner;
 
 public class Controller {
 
-  public static void main(String[] args) {
+    UI ui = new UI();
 
-    System.out.println("Welcome to this labyrint game!\nYou need to find your way out of this labyrint " +
+    public void mainMenu(){
+    ui.displayGreen("Welcome to this labyrint game!\nYou need to find your way out of this labyrint "+
             "by choosing your direction.");
+
     boolean keepRunning = true;
+
+
     do {
 
-       {
+        {
 
-        System.out.println("Choose your direction:\nN = North\nW = West\nS = South\nE = east\nPress Q for quit\n\n" +
-                "Please choose: ");
+            System.out.println("Choose your direction:\nN = North\nW = West\nS = South\nE = east\nPress Q for quit\n\n" +
+                    "Please choose: ");
 
-        Scanner sc = new Scanner(System.in);
+            Scanner sc = new Scanner(System.in);
 
-        String choose = sc.nextLine();
+            String choose = sc.nextLine();
 
-        switch (choose) {
-          case "N":
-            System.out.println("You choose N");
-            break;
-          case "W":
-            System.out.println("You choose W");
-            break;
-          case "S":
-            System.out.println("You choose S");
-            break;
-          case "E":
-            System.out.println("You choose E");
-            break;
-          case "Q":
-            System.out.println("You chosse to quit.. Goodbye!!");
-            System.exit(2);
-          default:
-            System.out.println("Please maake a valid choose!! Try again!");
+            switch (choose) {
+                case "N":
+                    north();
+
+                    break;
+                case "W":
+                    ui.displayGreen("You choose W");
+                    break;
+                case "S":
+                    ui.displayGreen("You choose S");
+                    break;
+                case "E":
+                    ui.displayGreen("You choose E");
+                    break;
+                case "Q":
+                    System.out.println("You chosse to quit.. Goodbye!!");
+                    System.exit(2);
+                default:
+                    System.out.println("Please maake a valid choose!! Try again!");
 
 
+            }
         }
-      }
 
-    } while (keepRunning == true);
+    } while(keepRunning ==true);
 
-      }
+    }
+
+
+    public void north(){
+
+
+    }
+    public void west(){
+
+
+    }
+    public void south(){
+
+
+    }
+    public void east(){
+
+
+    }
+
+
+
 
 }
